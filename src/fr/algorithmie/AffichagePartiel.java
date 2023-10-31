41e28d1bb16dfb7b149da37bca57b9b2359d8aff
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-10-31 15:15:57 +0100

    ajout

---
+++ src/fr/algorithmie/AffichagePartiel.java
@@ -0,0 +1,14 @@
+package fr.algorithmie;
+
+public class AffichagePartiel {
+    public static void main(String[] args) {
+        int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4};
+        for (int i=0; i<array.length; i++) {
+            if (array[i] > 3) {
+                System.out.println(array[i]);
+            }
+        }
+
+    }
+
+}
