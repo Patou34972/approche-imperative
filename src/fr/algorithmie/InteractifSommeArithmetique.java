439db40a30476629d26f69e384a68f1b464eaef5
patrice ELIE DIT COSAQUE <patriceedc@hotmail.com>
2023-11-04 18:07:29 +0100

    mise à jour

--- src/fr/algorithmie/InteractifSommeArithmetique.java
+++ src/fr/algorithmie/InteractifSommeArithmetique.java
@@ -2,17 +2,12 @@ package fr.algorithmie;
 
 import java.util.Scanner;
 
-public class InteractifChiffresSuivants {
+public class InteractifSommeArithmetique {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Veuillez saisir un nombre : ");
         int nombre = scanner.nextInt();
 
-        System.out.println("Les 10 nombres suivants sont :");
-
-        for (int i = nombre + 1; i <= nombre + 10; i++) {
-            System.out.println("Les 10 nombres suivants sont :" +i);
-        }
     }
 }
